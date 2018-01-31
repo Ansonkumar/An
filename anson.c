6ebe33368c9273ddddd9f087f14f2046b5ced828
Ansonkumar <35687544+Ansonkumar@users.noreply.github.com>
2018-01-31 15:07:21 +0530

    Create anson.c

---
+++ anson.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+	int e,f,g;
+	printf("Enter 3 numbers\n");
+	scanf("%d\n",&e);
+	scanf("%d\n",&f);
+	scanf("%d\n",&g);
+	if(e>f&&e>f)
+	{
+		printf("%d is greate\nr",e);
+	}
+	else if(f>e&&f>g)
+	{
+		printf("%d is greater\n",f);
+	}
+	else if(g>e&&g>f)
+	{
+		printf("%d is greater\n",g);
+	}
+}
